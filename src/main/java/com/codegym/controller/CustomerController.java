4d60aceeed78fec4e522a5d2056d464faf92c36a
DuyTran1811 <ferssken99@gmail.com>
2021-04-01 21:56:33 +0700

    firs commit

---
+++ src/main/java/com/codegym/controller/CustomerController.java
@@ -0,0 +1,91 @@
+package com.codegym.controller;
+
+import com.codegym.model.Customer;
+import com.codegym.model.Province;
+import com.codegym.service.CustomerService;
+import com.codegym.service.ProvinceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private ProvinceService provinceService;
+
+    @ModelAttribute("provinces")
+    public Iterable<Province> provinces() {
+        return provinceService.findAll();
+    }
+
+    @GetMapping("/customers")
+    public ModelAndView listCustomer() {
+        Iterable<Customer> customers = customerService.findAll();
+        ModelAndView modelAndView = new ModelAndView("/customer/list");
+        modelAndView.addObject("customers", customers);
+        return modelAndView;
+    }
+    @GetMapping("/create")
+    public ModelAndView showCreateForm(){
+        ModelAndView modelAndView = new ModelAndView("/customer/create");
+        modelAndView.addObject("customer", new Customer());
+        return modelAndView;
+    }
+
+    @PostMapping("/create")
+    public ModelAndView saveCustomer(@ModelAttribute("customer") Customer customer){
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("/customer/create");
+        modelAndView.addObject("customer", new Customer());
+        modelAndView.addObject("message", "New customer created successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView showEditForm(@PathVariable Long id){
+        Customer customer = customerService.findById(id);
+        ModelAndView modelAndView;
+        if(customer != null) {
+            modelAndView = new ModelAndView("/customer/edit");
+            modelAndView.addObject("customer", customer);
+        }else {
+            modelAndView = new ModelAndView("/error.404");
+        }
+        return modelAndView;
+    }
+
+    @PostMapping("/edit")
+    public ModelAndView updateCustomer(@ModelAttribute("customer") Customer customer){
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("/customer/edit");
+        modelAndView.addObject("customer", customer);
+        modelAndView.addObject("message", "Customer updated successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView showDeleteForm(@PathVariable Long id){
+        Customer customer = customerService.findById(id);
+        ModelAndView modelAndView;
+        if(customer != null) {
+            modelAndView = new ModelAndView("/customer/delete");
+            modelAndView.addObject("customer", customer);
+        }else {
+            modelAndView = new ModelAndView("/error.404");
+        }
+        return modelAndView;
+    }
+
+    @PostMapping("/delete")
+    public String deleteCustomer(@ModelAttribute("customer") Customer customer){
+        customerService.remove(customer.getId());
+        return "redirect:customers";
+    }
+}
---
+++ src/main/java/com/codegym/repository/ProvinceRepository.java
@@ -0,0 +1,10 @@
+package com.codegym.repository;
+
+import com.codegym.model.Province;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProvinceRepository extends PagingAndSortingRepository<Province, Long> {
+    Province findByName(String name);
+}
